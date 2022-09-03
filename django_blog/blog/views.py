e393ca2427b9498482a88c9352c838ab95d3f14f
ankit-bhadoriya <ankit.bhadoriya@viit.ac.in>
2022-09-03 22:57:57 +0530

    modified view ,url ,templates & added styling to templates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import  HttpResponse
 
-# Create your views here.
+
+posts=[{
+        "title":"My 1st Post",
+        "author":"animX",
+        "date_posted":"24th Feb 2019",
+        "content":"its a good day to be alive" },
+        {
+        "title":"My 2nd Post",
+        "author":"animX",
+        "date_posted":"24th Feb 2019",
+        "content":"its a good day to be alive" },
+        {
+        "title":"My 3rd Post",
+        "author":"animX",
+        "date_posted":"24th Feb 2019",
+        "content":"its a good day to be alive" }]
+
+def home(request):
+    # return HttpResponse('<h1> Welcome to Blog Home </h1>')
+    context={"posts":posts}
+    return render(request,'blog/home.html' , context)
+
+
+
+def about(request):
+    return render (request , "blog/about.html" , context={"title":"About Page"})
+
