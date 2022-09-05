6c790c451fd278e3b3a355293819daa2540e6f93
ankit-bhadoriya <ankit.bhadoriya@viit.ac.in>
2022-09-05 14:06:52 +0530

    added user registration app and models user and posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
-
-
-posts=[{
-        "title":"My 1st Post",
-        "author":"animX",
-        "date_posted":"24th Feb 2019",
-        "content":"its a good day to be alive" },
-        {
-        "title":"My 2nd Post",
-        "author":"animX",
-        "date_posted":"24th Feb 2019",
-        "content":"its a good day to be alive" },
-        {
-        "title":"My 3rd Post",
-        "author":"animX",
-        "date_posted":"24th Feb 2019",
-        "content":"its a good day to be alive" }]
+from .models import Post
 
 def home(request):
     # return HttpResponse('<h1> Welcome to Blog Home </h1>')
-    context={"posts":posts}
+    context={"posts":Post.objects.all()}
     return render(request,'blog/home.html' , context)
 
 
